ec371f5294324e4c9de7b88c8719a5ef17d89e0f
sanatanjena1 <sanatan.jena112@gmail.com>
2023-11-13 16:30:14 +0530

    add all student mark sheet in js file

--- scripts.js
+++ scripts.js
@@ -2,23 +2,473 @@ const studentData = [];
 
 // Generate student data for registration numbers 1 to 155
 for (let i = 2205297001; i <= 2205297155; i++) {
-    studentData.push({ registrationNumber: 2205297001, marksheet: "👇👇👇", photoURL: "img/s1.png" });
+    studentData.push({ registrationNumber: 2205297001, marksheet: "👇👇👇", photoURL: "s2img/s1.png" });
 }
 
 {
-    studentData.push({ registrationNumber: 2205297002, marksheet: "👇👇👇", photoURL: "img/s2.png" });
+    studentData.push({ registrationNumber: 2205297002, marksheet: "👇👇👇", photoURL: "s2img/s2.png" });
 }
 {
-    studentData.push({ registrationNumber: 2205297003, marksheet: "👇👇👇", photoURL: "img/s3.png" });
+    studentData.push({ registrationNumber: 2205297003, marksheet: "👇👇👇", photoURL: "s2img/s3.png" });
 }
 {
-    studentData.push({ registrationNumber: 2205297004, marksheet: "👇👇👇", photoURL: "img/s4.png" });
+    studentData.push({ registrationNumber: 2205297004, marksheet: "👇👇👇", photoURL: "s2img/s4.png" });
 }
 {
-    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "img/s5.png" });
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s5.png" });
 }
 
-
+{
+    studentData.push({ registrationNumber: 2205297006, marksheet: "👇👇👇", photoURL: "s2img/s6.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297007, marksheet: "👇👇👇", photoURL: "s2img/s7.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297008, marksheet: "👇👇👇", photoURL: "s2img/s8.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297009, marksheet: "👇👇👇", photoURL: "s2img/s9.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297010, marksheet: "👇👇👇", photoURL: "s2img/s10.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s11.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s12.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s13.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s14.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s15.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s16.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s17.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s18.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s19.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s20.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s21.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s22.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s23.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s24.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s25.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s26.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s27.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s28.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s29.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s30.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s31.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s32.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s33.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s34.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s35.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s36.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s37.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s38.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s39.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s40.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s41.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s42.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s43.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s44.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s45.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s46.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s47.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s48.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s49.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s50.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s51.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s52.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s53.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s54.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s55.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s56.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s57.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s58.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s59.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s60.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s61.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s62.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s63.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s64.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s65.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s66.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s67.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s68.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s69.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s70.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s71.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s72.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s73.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s74.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s75.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s76.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s77.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s78.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s79.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s80.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s81.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s82.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s83.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s84.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s85.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s86.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s87.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s88.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s89.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s90.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s91.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s92.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s93.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s94.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s95.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s96.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s97.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s98.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s99.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s100.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s101.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s102.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s103.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s104.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s105.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s106.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s107.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s108.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s109.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s110.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s111.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s112.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s113.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s114.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s115.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s116.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s117.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s118.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s119.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s120.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s121.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s122.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s123.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s124.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s125.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s126.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s127.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s128.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s129.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s130.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s131.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s132.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s133.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s134.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s135.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s136.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s137.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s138.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s139.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s140.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s141.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s142.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s143.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s144.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s145.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s146.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s147.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s148.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s149.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s150.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s151.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s152.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s153.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s154.png" });
+}
+{
+    studentData.push({ registrationNumber: 2205297005, marksheet: "👇👇👇", photoURL: "s2img/s155.png" });
+}
+    
 const resultContainer = document.querySelector(".result-container");
 const registrationInput = document.getElementById("registrationInput");
 const searchButton = document.getElementById("searchButton");
